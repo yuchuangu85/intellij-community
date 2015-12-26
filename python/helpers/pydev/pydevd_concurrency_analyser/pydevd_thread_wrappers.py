bf6c6d0d50a1a259dc5b43af52d25dda8f336203
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2015-12-26 18:12:37 +0300

    Changes from PyDev debugger

--- python/helpers/pydev/pydevd_concurrency_analyser/pydevd_thread_wrappers.py
+++ python/helpers/pydev/pydevd_concurrency_analyser/pydevd_thread_wrappers.py
@@ -1,5 +1,5 @@
 
-import _pydev_threading as threading
+from _pydev_imps import _pydev_threading as threading
 import functools
 
 
@@ -72,8 +72,9 @@ def wrap_threads():
 
     # queue patching
     try:
-        import queue
+        import queue  # @UnresolvedImport
         queue.Queue = factory_wrapper(queue.Queue)
     except:
         import Queue
         Queue.Queue = factory_wrapper(Queue.Queue)
+

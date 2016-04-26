3c8bd232fb7791f7f43d43627c3fe9b408dd480d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-04-26 10:29:48 +0200

    built-in web server — use own file to store token (cherry picked from commit a325ad3)

--- platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
+++ platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
@@ -197,7 +197,7 @@ public class ConfigImportHelper {
     FileUtil.copyDir(src, dest);
 
     // delete old user token - we must not reuse it
-    FileUtil.delete(new File(dest, "user.token"));
+    FileUtil.delete(new File(dest, "user.web.token"));
 
     File oldPluginsDir = new File(src, PLUGINS_PATH);
     if (!oldPluginsDir.isDirectory() && SystemInfo.isMac) {
